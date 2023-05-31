a19e4129d529e253c54c6edf09f6b42a63e5dc73
Brenden Thornton <thorntonbrenden@gmail.com>
2023-05-31 15:12:28 -0600

    styled button to match main site

--- src/components/button.js
+++ src/components/button.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import "./layout.css"
 
 const Button = (props) => {
@@ -16,9 +16,9 @@ const Button = (props) => {
       style={{
         padding: '5px 25px',
         margin: '10px 0',
-        borderRadius: '5px',
-        borderWidth: '1px',
-        backgroundColor: 'rgb(51, 135, 249, .8)',
+        borderRadius: '3px',
+        borderWidth: '0',
+        backgroundColor: 'rgb(123, 220, 124)',
         color: 'white'
       }}
     >
