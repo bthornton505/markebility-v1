cb8a7ca79ae8850cc52671da57fcc9abb31681ca
Brenden Thornton <thorntonbrenden@gmail.com>
2023-06-15 11:13:02 -0600

    trying to resolve build issues

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,49 +1,51 @@
 import React, { createContext, useState, useReducer } from 'react';
 import appReducer from '../reducers/appReducer';
 
-export const AppContext = createContext();
+const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(
-      appReducer,
-      {
-        userId: null,
-        email: '',
-        name: '',
-        recommendationReqId: null,
-        recommendationReq: {
-          nameOfBusiness: '',
-          businessOffering: '',
-          marketingGoal: '',
-          currentMarketingActivities: '',
-          typicalCustomer: '',
-          budgetTotal: ''
-        },
-        recommendation: ''
-      }
-    );
-
-    const updateUserState = (payload) => {
-      dispatch({
-        type: 'update_user',
-        payload
-      });
+  const [state, dispatch] = useReducer(
+    appReducer,
+    {
+      userId: null,
+      email: '',
+      name: '',
+      recommendationReqId: null,
+      recommendationReq: {
+        nameOfBusiness: '',
+        businessOffering: '',
+        marketingGoal: '',
+        currentMarketingActivities: '',
+        typicalCustomer: '',
+        budgetTotal: ''
+      },
+      recommendation: ''
     }
+  );
 
-    const updateRecommendationState = (payload) => {
-      dispatch({
-        type: 'update_recommendation',
-        payload
-      });
-    }
-  
-    const handleNav = () => {
-      console.log('hello from provider')
-    };
-  
-    return (
-      <AppContext.Provider value={{ state, dispatch, handleNav, updateRecommendationState, updateUserState}}>
-        {children}
-      </AppContext.Provider>
-    );
+  const updateUserState = (payload) => {
+    dispatch({
+      type: 'update_user',
+      payload
+    });
+  }
+
+  const updateRecommendationState = (payload) => {
+    dispatch({
+      type: 'update_recommendation',
+      payload
+    });
+  }
+
+  const handleNav = () => {
+    console.log('hello from provider')
   };
+
+  return (
+    <AppContext.Provider value={{ state, dispatch, handleNav, updateRecommendationState, updateUserState}}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContext
