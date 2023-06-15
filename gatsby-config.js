de912d60d88cc9b4ecaaed4ab131275b9420b23a
Brenden Thornton <thorntonbrenden@gmail.com>
2023-06-15 10:48:22 -0600

    trying to resolve build errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,10 +41,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-  ],
-  flags: {
-    DEV_SSR: true,
-  }
+  ]
 }
 
 require("dotenv").config({
